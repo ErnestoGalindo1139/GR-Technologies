b3c082473a9f55b32db2d44c6c52e7cc12167f25
Juan Carlos Rivas <juan_carlosr223@hotmail.com>
2024-02-17 01:22:28 -0700

    Clientes para el efecto del carrusel

--- src/data/clientes.js
+++ src/data/clientes.js
@@ -2,5 +2,6 @@ export const clientes = [
     { nombre: 'El Walamito', url: 'https://res.cloudinary.com/dfszyib7k/image/upload/v1708149261/grs-pagina-web/el-walamito_ck2rnw.png' },
     { nombre: 'Misso Sushi', url: 'https://res.cloudinary.com/dfszyib7k/image/upload/v1708149381/grs-pagina-web/Misso-sushi_aodbs8.png' },
     { nombre: 'Yarda El Marino', url: 'https://res.cloudinary.com/dfszyib7k/image/upload/v1708151684/grs-pagina-web/yarda-el-marino_fpg7m0.png' },
-    
+    { nombre: 'El Walamito', url: 'https://res.cloudinary.com/dfszyib7k/image/upload/v1708149261/grs-pagina-web/el-walamito_ck2rnw.png' },
+    { nombre: 'Misso Sushi', url: 'https://res.cloudinary.com/dfszyib7k/image/upload/v1708149381/grs-pagina-web/Misso-sushi_aodbs8.png' },
 ];
